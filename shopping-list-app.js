982866a6b93c83bc771b2d3a82a29ba9596dc0e4
Gene Barra <Gebarra9@gmail.com>
2020-07-12 15:54:21 -0400

    add:modals for delete local storage

--- shopping-list-app.js
+++ shopping-list-app.js
@@ -5,3 +5,44 @@ const headerContainer = document.querySelector(".header-container");
 const dateText = document.createElement("p");
 dateText.innerText = `Today is ${moment().format("dddd, MMMM DD, YYYY")}`;
 headerContainer.appendChild(dateText);
+
+/* MODALS */
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
+
+openModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+console.log(closeModalButtons);
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    //closest --> selects closest parent with specified class
+    const modal = button.closest(".delete-storage-modal");
+    closeModal(modal);
+  });
+});
+
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".modal.active");
+  console.log(modals);
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
