39d554e05c3d264d1c299722256a23def6b21122
Gene Barra <Gebarra9@gmail.com>
2020-07-12 22:45:33 -0400

    add:favicon and additional modal styling

--- shopping-list-app.js
+++ shopping-list-app.js
@@ -1,5 +1,8 @@
 /* DECLARE GLOBAL VARIABLES */
 const headerContainer = document.querySelector(".header-container");
+const squareBox = document.querySelector("#square-box");
+
+const itemText = document.querySelector(".item-text");
 
 /* MOMENT JS */
 const dateText = document.createElement("p");
@@ -11,6 +14,22 @@ const openModalButtons = document.querySelectorAll("[data-modal-target]");
 const closeModalButtons = document.querySelectorAll("[data-close-button]");
 const overlay = document.getElementById("overlay");
 
+/* START - CHECKBOX */
+squareBox.addEventListener("click", (event) => {
+  const squareBoxState = event.target;
+  console.log(itemText);
+  console.log(squareBoxState.className.includes("fa-square"));
+  if (squareBoxState.className.includes("fa-square")) {
+    return checkBox(squareBoxState, itemText);
+  }
+  if (squareBoxState.className.includes("fa-check-square")) {
+    return uncheckBox(squareBoxState, itemText);
+  }
+});
+
+/* END - CHECKBOX */
+
+// Add Click Event to All elements with Modal Target and passes value to openModal Function
 openModalButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const modal = document.querySelector(button.dataset.modalTarget);
@@ -22,12 +41,13 @@ console.log(closeModalButtons);
 closeModalButtons.forEach((button) => {
   button.addEventListener("click", () => {
     //closest --> selects closest parent with specified class
-    const modal = button.closest(".delete-storage-modal");
+    const modal = button.closest(".modal");
     closeModal(modal);
   });
 });
 
 overlay.addEventListener("click", () => {
+  console.log("test");
   const modals = document.querySelectorAll(".modal.active");
   console.log(modals);
   modals.forEach((modal) => {
@@ -35,14 +55,4 @@ overlay.addEventListener("click", () => {
   });
 });
 
-function openModal(modal) {
-  if (modal == null) return;
-  modal.classList.add("active");
-  overlay.classList.add("active");
-}
-
-function closeModal(modal) {
-  if (modal == null) return;
-  modal.classList.remove("active");
-  overlay.classList.remove("active");
-}
+console.log(overlay);
