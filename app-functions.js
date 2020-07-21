49a84d369c41c8232f4cb277e002797b6f02260e
Gbarra9 <gebarra9@gmail.com>
2020-07-21 01:51:33 -0400

    add: refresh load when delete local storage

--- app-functions.js
+++ app-functions.js
@@ -1,37 +1,37 @@
-"use strict";
+"use strict"
 /* START - RETRIEVE LOCAL STORAGE SHOPPING ITEMS  */
 
 const getShoppingList = () => {
-  const itemsJSON = localStorage.getItem("list");
+  const itemsJSON = localStorage.getItem("list")
   try {
-    return itemsJSON ? JSON.parse(itemsJSON) : [];
+    return itemsJSON ? JSON.parse(itemsJSON) : []
   } catch {
-    return [];
+    return []
   }
-};
+}
 
 /* END - RETRIEVE LOCAL STORAGE SHOPPING ITEMS */
 
 /* START - SAVE TO LOCAL STORAGE AND STRINGIFY OBJECT */
 const saveItems = (items) => {
-  localStorage.setItem("list", JSON.stringify(items));
-};
+  localStorage.setItem("list", JSON.stringify(items))
+}
 /* END - SAVE TO LOCAL STORAGE */
 
 /*  START - RENDER LIST ITEMS */
 // TODO: RENDER LIST BY GENERATING DOM
 const renderListItems = (items) => {
-  console.log(items);
-  console.log("list items not empty");
+  console.log(items)
+  console.log("list items not empty")
 
-  const emptyShoppingList = document.querySelector(".empty-shopping-list");
+  const emptyShoppingList = document.querySelector(".empty-shopping-list")
   if (emptyShoppingList) {
-    emptyShoppingList.remove();
+    emptyShoppingList.remove()
   }
 
-  const listContainer = document.querySelector(".list-container");
-  const unorderedList = document.createElement("ul");
-  unorderedList.classList.add("list-items");
+  const listContainer = document.querySelector(".list-container")
+  const unorderedList = document.createElement("ul")
+  unorderedList.classList.add("list-items")
   items.forEach((item) => {
     // debugger;
     unorderedList.innerHTML = `<li class="item" id=${item.id}>
@@ -49,26 +49,26 @@ const renderListItems = (items) => {
         class="fas fa-trash trash-button"
       ></i>
       </span>
-    </li>`;
-  });
-  listContainer.appendChild(unorderedList);
+    </li>`
+  })
+  listContainer.appendChild(unorderedList)
 
   // debugger;
-  saveItems(items);
-};
+  saveItems(items)
+}
 /*  END - RENDER LIST ITEMS */
 
 /*  START - POPUP MODAL FUNCTIONS */
 function openModal(modal) {
-  if (modal === null) return;
-  modal.classList.add("active");
-  overlay.classList.add("active");
+  if (modal === null) return
+  modal.classList.add("active")
+  overlay.classList.add("active")
 }
 
 function closeModal(modal) {
-  if (modal === null) return;
-  modal.classList.remove("active");
-  overlay.classList.remove("active");
+  if (modal === null) return
+  modal.classList.remove("active")
+  overlay.classList.remove("active")
 }
 
 /*  END - POPUP MODAL FUNCTIONS */
@@ -76,27 +76,28 @@ function closeModal(modal) {
 /* START - CHECKBOX FUNCTIONALITY */
 
 const checkBox = (squareBoxState, itemText) => {
-  if (squareBoxState === null) return;
-  squareBoxState.classList.remove("fa-square");
-  squareBoxState.classList.add("fa-check-square");
+  if (squareBoxState === null) return
+  squareBoxState.classList.remove("fa-square")
+  squareBoxState.classList.add("fa-check-square")
   // itemText.classList.add("strike-text");
-};
+}
 
 const uncheckBox = (squareBoxState, itemText) => {
-  if (squareBoxState === null) return;
-  squareBoxState.classList.remove("fa-check-square");
-  squareBoxState.classList.add("fa-square");
+  if (squareBoxState === null) return
+  squareBoxState.classList.remove("fa-check-square")
+  squareBoxState.classList.add("fa-square")
   // itemText.classList.remove("strike-text");
-};
+}
 
 /* END - CHECKBOX FUNCTIONALITY */
 
 /* START - DELETE LOCAL STORAGE */
 const deleteLocalStorageConfirmButton = document.querySelector(
   ".delete-confirm-local-storage-button"
-);
+)
 deleteLocalStorageConfirmButton.addEventListener("click", (event) => {
-  localStorage.removeItem("list");
-});
+  localStorage.removeItem("list")
+  location.reload()
+})
 
 /* END - DELETE LOCAL STORAGE */
