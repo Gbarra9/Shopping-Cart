e6ef8b680ddb5501dbe8526bdca6872b88cab0da
Gene Barra <Gebarra9@gmail.com>
2020-08-12 17:29:12 -0400

    Add: delete, add, reset local storage functionality

--- app-functions.js
+++ app-functions.js
@@ -1,106 +1,101 @@
 "use strict";
 /* START - RETRIEVE LOCAL STORAGE SHOPPING ITEMS  */
 
-const getShoppingList = () => {
-  const itemsJSON = localStorage.getItem("list");
-  try {
-    return itemsJSON ? JSON.parse(itemsJSON) : [];
-  } catch {
-    return [];
-  }
-};
+// const getShoppingList = () => {
+//   const itemsJSON = localStorage.getItem("list");
+//   try {
+//     return itemsJSON ? JSON.parse(itemsJSON) : [];
+//   } catch {
+//     return [];
+//   }
+// };
 
 /* END - RETRIEVE LOCAL STORAGE SHOPPING ITEMS */
 
 /* START - SAVE TO LOCAL STORAGE AND STRINGIFY OBJECT */
-const saveItems = (items) => {
-  localStorage.setItem("list", JSON.stringify(items));
-};
+
+// const saveItems = (items) => {
+//   localStorage.setItem("list", JSON.stringify(items));
+// };
+
 /* END - SAVE TO LOCAL STORAGE */
 
 /*  START - RENDER LIST ITEMS */
 // TODO: RENDER LIST BY GENERATING DOM
-const renderListItems = (items) => {
-  console.log(items);
-  console.log("list items not empty");
 
-  const emptyShoppingList = document.querySelector(".empty-shopping-list");
-  if (emptyShoppingList) {
-    emptyShoppingList.remove();
-  }
+// const renderListItems = (items) => {
+//   console.log(items);
+//   console.log("list items not empty");
 
-  items.forEach((item) => {
-    const unorderedList = document.querySelector(".list-items");
-    const listItem = document.createElement("li");
-    listItem.classList.add("item");
-    listItem.id = item.id;
+//   const emptyShoppingList = document.querySelector(".empty-shopping-list");
+//   if (emptyShoppingList) {
+//     emptyShoppingList.remove();
+//   }
 
-    listItem.innerHTML = `
-    <span class="check-square-item-span">
-    <i id="square-box" class="far fa-square ${item.checked}"></i>
-    <p class="item-text">${item.text}</p>
-    </span>
-    <span class="edit-remove-span">
-    <i
-    data-modal-target="#edit-modal"
-    class="fas fa-edit edit-button"
-    ></i>
-    <i
-    data-modal-target=${item.id}
-    class="fas fa-trash trash-button"
-    ></i>
-    </span>`;
+//   items.forEach((item) => {
+//     const unorderedList = document.querySelector(".list-items");
+//     const listItem = document.createElement("li");
+//     listItem.classList.add("item");
+//     listItem.id = item.id;
 
-    unorderedList.appendChild(listItem);
-    const trashButton = document.querySelector(".trash-button");
-    trashButton.addEventListener("click", (event) => {
-      console.log(event.target.value);
-    });
-  });
-};
+//     listItem.innerHTML = `
+//     <span class="check-square-item-span">
+//     <i id="square-box" class="far fa-square ${item.checked}"></i>
+//     <i class="far fa-check-square"></i>
+//     <p class="item-text">${item.text}</p>
+//     </span>
+//     <span class="edit-remove-span">
+//     <i
+//     class="fas fa-trash trash-button"
+//     ></i>
+//     </span>`;
+
+//     unorderedList.appendChild(listItem);
+//     const trashButton = document.querySelector(".trash-button");
+//     trashButton.addEventListener("click", (event) => {
+//       console.log(event.target.value);
+//     });
+//   });
+// };
 
 /*  END - RENDER LIST ITEMS */
 
-function addItem(id, text, timeStamp) {
-  const item = {
-    id: id,
-    text: text,
-    checked: false,
-    createdAt: timeStamp,
-    updatedAt: timeStamp,
-  };
-  listItems.push(item);
-  generateItemDOM(item);
-}
+// function addItem(id, text, timeStamp) {
+//   const item = {
+//     id: id,
+//     text: text,
+//     checked: false,
+//     createdAt: timeStamp,
+//     updatedAt: timeStamp,
+//   };
+//   listItems.push(item);
+//   generateItemDOM(item);
+// }
 
-function generateItemDOM(item) {
-  const emptyShoppingList = document.querySelector(".empty-shopping-list");
-  if (emptyShoppingList) {
-    emptyShoppingList.remove();
-  }
+// function generateItemDOM(item) {
+//
+//   if (emptyShoppingList) {
+//     emptyShoppingList.remove();
+//   }
 
-  const unorderedList = document.querySelector(".list-items");
-  const listItem = document.createElement("li");
-  listItem.classList.add("item");
-  listItem.id = item.id;
+//   const unorderedList = document.querySelector(".list-items");
+//   const listItem = document.createElement("li");
+//   listItem.classList.add("item");
+//   listItem.id = item.id;
 
-  listItem.innerHTML = `
-  <span class="check-square-item-span">
-  <i id="square-box" class="far fa-square ${item.checked}"></i>
-  <p class="item-text">${item.text}</p>
-  </span>
-  <span class="edit-remove-span">
-  <i
-  data-modal-target="#edit-modal"
-  class="fas fa-edit edit-button"
-  ></i>
-  <i
-  data-modal-target="#delete-item-modal"
-  class="fas fa-trash trash-button"
-  ></i>
-  </span>`;
-  unorderedList.appendChild(listItem);
-}
+//   listItem.innerHTML = `
+//   <span class="check-square-item-span">
+//   <i id="square-box" class="far fa-square ${item.checked}"></i>
+//   <p class="item-text">${item.text}</p>
+//   </span>
+//   <span class="edit-remove-span">
+//   <i
+//   data-modal-target="#delete-item-modal"
+//   class="fas fa-trash trash-button"
+//   ></i>
+//   </span>`;
+//   unorderedList.appendChild(listItem);
+// }
 
 /*  START - POPUP MODAL FUNCTIONS */
 function openModal(modal) {
@@ -117,52 +112,57 @@ function closeModal(modal) {
 
 /*  END - POPUP MODAL FUNCTIONS */
 
-/* START - CHECKBOX FUNCTIONALITY */
-
-const checkBox = (squareBoxState, itemText) => {
-  if (squareBoxState === null) return;
-  squareBoxState.classList.remove("fa-square");
-  squareBoxState.classList.add("fa-check-square");
-  // itemText.classList.add("strike-text");
-};
-
-const uncheckBox = (squareBoxState, itemText) => {
-  if (squareBoxState === null) return;
-  squareBoxState.classList.remove("fa-check-square");
-  squareBoxState.classList.add("fa-square");
-  // itemText.classList.remove("strike-text");
-};
-
-/* END - CHECKBOX FUNCTIONALITY */
-
 /* START - DELETE LOCAL STORAGE */
 const deleteLocalStorageConfirmButton = document.querySelector(
   ".delete-confirm-local-storage-button"
 );
 deleteLocalStorageConfirmButton.addEventListener("click", (event) => {
-  listItems = getShoppingList();
-
-  if (listItems.length > 0) {
+  if (listItems.length) {
+    localStorage.removeItem("List");
     const listContainer = document.querySelector(".list-container");
-    localStorage.removeItem("list");
     const emptyList = document.createElement("div");
     const listItem = document.querySelector(".list-items");
     listItem.innerHTML = "";
     emptyList.className = "empty-shopping-list";
     emptyList.innerHTML = `
-  <i
-    id="shopping-cart-icon"
-    class="fas fa-shopping-cart"
-    aria-label="shopping cart icon"
-  ></i>
-  <h3 class="empty-shopping-list-header">Add your first item</h2>
-  <p class="empty-shopping-list-text">What do you plan to buy today?</p>
-`;
+    <i
+      id="shopping-cart-icon"
+      class="fas fa-shopping-cart"
+      aria-label="shopping cart icon"
+    ></i>
+    <h3 class="empty-shopping-list-header">Add your first item</h2>
+    <p class="empty-shopping-list-text">What do you plan to buy today?</p>
+  `;
     listContainer.prepend(emptyList);
+    listItems = getShopList();
+    if (!listItems.length) {
+      loadShopList(listItems);
+    }
   }
-  console.log(listItems);
-  listItems;
+
   // location.reload();
 });
 
 /* END - DELETE LOCAL STORAGE */
+
+const sortItems = function (list, sortBy) {
+  debugger;
+  if (sortBy === "byCreated") {
+    return list.sort((a, b) => {
+      if (a.createdAt < b.createdAt) {
+        return -1;
+      } else if (a.createdAt > b.createdAt) {
+        return 1;
+      } else return 0;
+    });
+  } else if (sortBy === "byAlphabetical") {
+    return list.sort((a, b) => {
+      if (a.text.toLowerCase() < b.text.toLowerCase()) {
+        return -1;
+      } else if (a.text.toLowerCase() > b.text.toLowerCase()) {
+        return 1;
+      } else return 0;
+    });
+  }
+  return list;
+};
--- shopping-list-app.js
+++ shopping-list-app.js
@@ -1,8 +1,6 @@
 "use strict";
 /* START - DECLARE GLOBAL VARIABLES */
 const headerContainer = document.querySelector(".header-container");
-const squareBox = document.querySelector("#square-box");
-const filterDropdown = document.querySelector("#filter-by");
 
 const deleteLocalStorage = document.querySelector(
   ".delete-confirm-local-storage-button"
@@ -16,12 +14,72 @@ const deleteConfirmItemButton = document.querySelector(
   ".delete-item-confirm-item-button"
 );
 
+const filterList = document.querySelector("#filter-by");
+
+const check = "fa-check-square";
+const uncheck = "fa-square";
+const strikeText = "strike-text";
+const listElement = document.querySelector(".list-items");
+const emptyShoppingList = document.querySelector(".empty-shopping-list");
+
 // IMPORTANT CALLS FUNCTION RETRIEVING DATA FROM LOCAL STORAGE
-let listItems = getShoppingList();
-console.log(listItems);
-if (listItems.length > 0) {
-  renderListItems(listItems);
+// let listItems = getShoppingList();
+// console.log(listItems);
+// if (listItems.length > 0) {
+//   renderListItems(listItems);
+// }
+
+const getShopList = function () {
+  const itemsJSON = localStorage.getItem("List");
+  try {
+    return itemsJSON ? JSON.parse(itemsJSON) : [];
+  } catch {
+    return [];
+  }
+};
+
+let listItems = getShopList();
+let filterBy = { sortby: "byCreated" };
+
+const loadShopList = function (listArr, filterBy) {
+  let list = sortItems(listArr, filterBy);
+
+  list.forEach((item) => {
+    addToList(
+      item.itemText,
+      item.itemId,
+      item.completed,
+      item.trash,
+      item.createdAt
+    );
+  });
+};
+
+if (listItems.length) {
+  loadShopList(listItems);
 }
+
+function checkEmptyList(listItems) {
+  if (listItems.length < 1) {
+    const listContainer = document.querySelector(".list-container");
+    localStorage.removeItem("List");
+    const emptyList = document.createElement("div");
+    const listItem = document.querySelector(".list-items");
+    listItem.innerHTML = "";
+    emptyList.className = "empty-shopping-list";
+    emptyList.innerHTML = `
+  <i
+    id="shopping-cart-icon"
+    class="fas fa-shopping-cart"
+    aria-label="shopping cart icon"
+  ></i>
+  <h3 class="empty-shopping-list-header">Add your first item</h2>
+  <p class="empty-shopping-list-text">What do you plan to buy today?</p>
+`;
+    listContainer.prepend(emptyList);
+  }
+}
+
 // IMPORTANT CALLS FUNCTION RETRIEVING DATA FROM LOCAL STORAGE
 
 const submitShoppingItem = document.querySelector("#submit-shopping-item");
@@ -45,30 +103,15 @@ const closeModalButtons = document.querySelectorAll("[data-close-button]");
 const overlay = document.getElementById("overlay");
 
 /* START - CHECKBOX */
-if (squareBox !== null) {
-  squareBox.addEventListener("click", (event) => {
-    const squareBoxState = event.target;
-    // console.log(itemText);
-    console.log(squareBoxState.className.includes("fa-square"));
-    if (squareBoxState.className.includes("fa-square")) {
-      return checkBox(squareBoxState);
-    }
-    if (squareBoxState.className.includes("fa-check-square")) {
-      return uncheckBox(squareBoxState);
-    }
-  });
-}
+
 /* END - CHECKBOX */
 
 /* START -  CRUD OPERATION FOR LOCAL STORAGE */
 
 submitShoppingItem.addEventListener("submit", (event) => {
   event.preventDefault();
-  const id = uuidv4();
   const text = event.target.elements.addItemInputField.value.trim();
-  const timeStamp = moment().valueOf();
   // console.log(event.target.elements.addItemInputField.value);
-  // console.log(timeStamp);
 
   // listItems.push({
   //   id: id,
@@ -78,10 +121,110 @@ submitShoppingItem.addEventListener("submit", (event) => {
   //   updatedAt: timeStamp,
   // });
 
-  addItem(id, text, timeStamp);
+  // addItem(id, text, timeStamp);
+  const emptyShoppingList = document.querySelector(".empty-shopping-list");
+
+  if (text) {
+    const id = uuidv4();
+    const timeStamp = moment().valueOf();
+    console.log(text, id, timeStamp);
+    listItems.push({
+      itemText: text,
+      itemId: id,
+      completed: false,
+      trash: false,
+      createdAt: timeStamp,
+    });
+    localStorage.setItem("List", JSON.stringify(listItems));
+    if (listItems.length && emptyShoppingList) {
+      emptyShoppingList.remove();
+    }
+    addToList(text, id, false, false, timeStamp, timeStamp);
+  }
+
   console.log(listItems);
-  saveItems(listItems);
+  // saveItems(listItems);
   event.target.elements.addItemInputField.value = "";
+});
+
+function addToList(itemText, itemId, completed, trash, createdAt) {
+  const done = completed ? check : uncheck;
+  const line = completed ? strikeText : "";
+
+  if (emptyShoppingList) {
+    emptyShoppingList.remove();
+  }
+
+  // If trash is true do not run code below
+  if (trash) {
+    return;
+  }
+
+  const listItemContent = `
+  <li id=${itemId} class="item" task=""> 
+    <span class="check-square-item-span" task="">
+      <i class="far ${done}" task="complete"></i>
+      <p class="item-text ${line}" task="">${itemText}</p>
+      </span>
+      <span class="edit-remove-span" task="">
+      <i
+      class="fas fa-trash trash-button" task="delete"
+      ></i>
+    </span>
+  </li>
+  `;
+  const position = "beforeend";
+  listElement.insertAdjacentHTML(position, listItemContent);
+}
+
+function completedItem(element) {
+  element.classList.toggle(check);
+  element.classList.toggle(uncheck);
+  element.parentNode.querySelector(".item-text").classList.toggle(strikeText);
+  const findItemIndex = listItems.findIndex((item) => {
+    return item.itemId === element.parentNode.parentNode.id;
+  });
+  // listItems[element.id].completed ? false : true;
+
+  listItems[findItemIndex].completed
+    ? (listItems[findItemIndex].completed = false)
+    : (listItems[findItemIndex].completed = true);
+  localStorage.setItem("List", JSON.stringify(listItems));
+  console.log(listItems);
+}
+
+function removeItem(element) {
+  console.log(element.parentNode.parentNode.parentNode);
+  console.log(element.parentNode.parentNode);
+  element.parentNode.parentNode.parentNode.removeChild(
+    element.parentNode.parentNode
+  );
+  console.log(element);
+  const findItemIndex = listItems.findIndex((item) => {
+    return item.itemId === element.parentNode.parentNode.id;
+  });
+  listItems[findItemIndex].trash = true;
+  listItems.splice(findItemIndex, 1);
+  localStorage.setItem("List", JSON.stringify(listItems));
+  console.log(listItems);
+  checkEmptyList(listItems);
+  // listItems[element.id].trash = true;
+}
+
+listElement.addEventListener("click", (event) => {
+  let element = event.target;
+  const elementTask = event.target.attributes.task.value;
+  if (elementTask === "delete") {
+    removeItem(element);
+  } else if (elementTask === "complete") {
+    completedItem(element);
+  }
+});
+
+filterList.addEventListener("change", (event) => {
+  console.log(event.target.value);
+  filterBy.sortby = event.target.value;
+  console.log(filterBy);
 });
 
 /* END - ONCLICK BUTTON CRUD OPERATION FOR LOCAL STORAGE */
@@ -94,7 +237,6 @@ openModalButtons.forEach((button) => {
     openModal(modal);
   });
 });
-// console.log(closeModalButtons);
 
 closeModalButtons.forEach((button) => {
   button.addEventListener("click", () => {
